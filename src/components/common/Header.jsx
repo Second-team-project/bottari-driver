73906fcd5914db231696f606f6e58076e60bd9ce
kimwemin <kimwemin488@gmail.com>
2026-01-14 10:47:45 +0900

    260114 로그아웃 에러 메세지 수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
       navigate('/login', { replace: true });
       toast.success("로그아웃 되었습니다.");
     } catch (error) {
-      toast.error("로그아웃에 실패했습니다. 잠시 후 다시 시도해주세요.");
+      toast.error(error.massege);
     }
   }
 
